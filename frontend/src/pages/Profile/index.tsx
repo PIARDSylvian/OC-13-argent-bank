ec928ec6e63b069fe5448519e5187e9a001bef5b
PIARDSylvian <piard.sylvian@gmail.com>
2023-06-04 22:12:21 +0200

    redirect if not login

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,10 +1,17 @@
 import style from './style.module.scss'
-
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectToken } from '../../redux/selector'
 
 export default function Profile() {
   const token = useSelector(selectToken)
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }
+  }, [navigate, token])
 
   return <>token {token}</>
 }
